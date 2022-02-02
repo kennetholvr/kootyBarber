71761414f482017eb00d5b96c8cdef1fabb0b143
kennetholvr <kennetholvr@gmail.com>
2022-02-02 11:40:34 -0600

    fixes bugs for working user login

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 // import logo from './logo.svg';
 import './App.css';
 import { Route, Routes} from "react-router-dom";
-import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+import { withAuthenticator} from "@aws-amplify/ui-react";
+import Amplify, { Auth } from "aws-amplify";
+import awsconfig from "./aws-exports";
 import SignIn from "./components/signIn";
 import PricingContent from './components/pricing';
 import Reservations from './components/reservations';
 import Availability from './components/availibility';
 
+Amplify.configure(awsconfig);
+Auth.configure(awsconfig);
+
 function App() {
   let authorization = true;
   return (
     <div className="App">
-      <AmplifySignOut />
       <Routes className="appContainer">
         <Route path="/" element={<SignIn />} />
         <Route
